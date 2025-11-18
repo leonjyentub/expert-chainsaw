4f9cc6957036fa67e07a46ef64ea8ab2fd68e239
leonjyentub <leonjye@ntub.edu.tw>
2025-11-18 14:46:25 +0800

    Refactor admin interface to include Profile management and update add_book template to use form.as_p

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import Profile
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profile'
+    fk_name = 'user'
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'profile_picture')
+    search_fields = ('user__username', 'user__email')
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+
+# Unregister the original User admin and register the customised one
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+# Also register Profile for direct access/managing
+admin.site.register(Profile, ProfileAdmin)
